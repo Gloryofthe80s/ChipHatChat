eea226564924659641ad882f7aac41aa2f07ab8e
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-02-20 11:08:12 -0500

    first commit.

---
+++ app/scripts/message-model.js
@@ -0,0 +1,25 @@
+//objects being sent to the server
+var Message = Backbone.Model.extend({
+    idAttribute: "_id",
+
+    defaults: {
+    username: 'string',
+    messageText: '-default message-',
+    messageDate: '-default date-', //use underscore's 'now'
+    appId: 'ChipHatApp' //our team name
+    options: '' //optional additional parameters
+
+    }
+});
+
+var MessagesCollection = Backbone.Collection.extend({
+    model: Message,
+
+    url: 'http://tiny-pizza-server.herokuapp.com'
+    // /collections/messages is the directory on server
+
+    // to see all messages on the server, you can run the following in console:
+    // $.get('/collections/contacts', function(result){
+    //    console.table(result);
+    // })
+})
